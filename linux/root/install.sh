0def8ea8c633209d36ab2a2546a373751d7d62ce
Andrii Trotsiuk <andriy.trotsyuk@gmail.com>
2022-11-10 15:57:13 +0200

    fix: linux installation

--- linux/root/install.sh
+++ linux/root/install.sh
@@ -1,5 +1,5 @@
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-cp -r "$scriptDir/airalarm" ~/.local/opt/
+cp -r "$scriptDir/airalarm" ~/.local/opt/airalarm
 mkdir -p ~/.local/share/applications/
 cp "$scriptDir/airalarm.desktop" ~/.local/share/applications/
 sed -i "2iIcon=$HOME/.local/opt/airalarm/megaphone.svg\nExec=$HOME/.local/opt/airalarm/airalarm" ~/.local/share/applications/airalarm.desktop
