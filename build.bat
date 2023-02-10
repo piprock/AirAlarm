236953b2c467a2540b142bf6623a326ad854f983
piprock <103489591+piprock@users.noreply.github.com>
2023-02-10 11:21:56 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 pyinstaller --add-data="src\data;data" --onefile --noconsole -i win32\megaphone.ico src\airalarm.py
 copy win32\root dist
 copy LICENSE dist
-set name=airalarm_linux_v%1
+set name=airalarm_windows_v%1
 rename dist "%name%"
 tar -cf "%name%.zip" "%name%"
 rmdir /S /Q build
